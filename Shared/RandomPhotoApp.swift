b61fbb219352809e07cc0eb8b36971ade05f1b42
Trinity Key <hellotrinitykey@gmail.com>
2022-04-16 00:14:22 -0400

    Initial Commit

---
+++ Shared/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  Shared
+//
+//  Created by Trinity Key on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
