b18ec5ab8e51abd3ca0f3490fb37536175fcaf4c
Trinity Key <hellotrinitykey@gmail.com>
2022-04-16 16:47:53 -0400

    adding a clickable button

--- RandomPhoto/RandomPhoto/ViewController.swift
+++ RandomPhoto/RandomPhoto/ViewController.swift
@@ -16,6 +16,16 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    
+    
     
 
     override func viewDidLoad() {
@@ -25,9 +35,22 @@ class ViewController: UIViewController {
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
         
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         getRandomPhoto()
         // Do any additional setup after loading the view.
     }
+    
+    @objc func didTapButton(){
+        getRandomPhoto()
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x: 20, y: view.frame.size.height-50-view.safeAreaInsets.bottom, width: view.frame.size.width-40, height: 50)
+    }
+    
     
     func getRandomPhoto() {
         let urlString = "https://source.unsplash.com/random/600x600"
